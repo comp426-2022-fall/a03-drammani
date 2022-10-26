c56cae14e97ead6db6ceb201e237943f07e7242b
drammani <drammani@live.unc.edu>
2022-10-26 15:05:55 -0400

    First try

---
+++ bin/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { roll } from "/lib/roll.js"
+import minimist from "minimist"
+
+var argv = minimist(process.argv.slice(2));
+
+const sides = argv.sides || 6
+const dice = argv.dice || 2
+const rolls = argv.rolls || 1
+
+console.log(JSON.stringify(roll(sides, dice, rolls)))
+
+
